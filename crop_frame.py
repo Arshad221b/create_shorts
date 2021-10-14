7665056b8a410beb187063bb72c7d0ceeb512294
arshad_221b <kaziarshad97@gmail.com>
2021-10-15 00:01:45 +0530

    get 9:16

--- crop_frame.py
+++ crop_frame.py
@@ -1,5 +1,4 @@
 from detect_face import DetectFace
-import cv2
 # crop frame
 #     |- crop get the required the width 
 #     |- get the face data 
@@ -10,9 +9,9 @@ class CropFrame():
         self.face   = face
 
     def get_face_data(self):
-        # x, _, _, _ = self.face['box']
-        x, y, w, h = self.face['box']
-        cv2.rectangle(self.img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        x, _, _, _ = self.face['box']
+        # x, y, w, h = self.face['box']
+        # cv2.rectangle(self.img, (x, y), (x+w, y+h), (255, 0, 0), 2)
         return x
 
     def crop_fame(self):
@@ -23,9 +22,9 @@ class CropFrame():
 
         return crop_img 
 
-import cv2
-img  = cv2.imread('test.jpg')
-face = DetectFace(img).detect_req_face()
-c = CropFrame(img, face).crop_fame()
-cv2.imshow("c", c)
-cv2.waitKey()
+# import cv2
+# img  = cv2.imread('test.jpg')
+# face = DetectFace(img).detect_req_face()
+# c = CropFrame(img, face).crop_fame()
+# cv2.imshow("c", c)
+# cv2.waitKey()
