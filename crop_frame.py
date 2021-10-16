067c3fa7509a1ef78b4c8db41a5fe3d8d5b2f7c0
arshad_221b <kaziarshad97@gmail.com>
2021-10-17 00:45:28 +0530

    save correct frame

--- crop_frame.py
+++ crop_frame.py
@@ -3,15 +3,24 @@ from detect_face import DetectFace
 #     |- crop get the required the width 
 
 class CropFrame():
-    def __init__(self, img, x) -> None:
+    def __init__(self, img, x, req_width) -> None:
         self.img    = img
         self.x      = x
+        self.req_width = req_width
 
 
     def crop_fame(self):
         width, height   = len(self.img), len(self.img[0])
-        req_width       = int(width*9/32)                                  # based on the height what should be the width (9:16 ratio)
-        crop_img        = self.img[:, self.x-req_width : self.x+req_width]
+        # req_width       = int(width*9/32)                                  # based on the height what should be the width (9:16 ratio)
+        # print(self.x + self.req_width, self.x - self.req_width)
+        if self.x + self.req_width < width and self.x - self.req_width > 0:
+            crop_img  = self.img[:, self.x- self.req_width : self.x+ self.req_width]
+        
+        elif self.x + self.req_width < width and self.x - self.req_width < 0:
+            crop_img  = self.img[:, 0: 2 * self.req_width]
+
+        elif self.x + self.req_width > width and self.x - self.req_width > 0:
+            crop_img  = self.img[:, width - 2 * self.req_width: width]
 
         return crop_img 
 
