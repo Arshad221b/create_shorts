3c23333e5cc38843c3e53ffae3abe9697b5096e1
arshad_221b <kaziarshad97@gmail.com>
2021-10-17 00:46:07 +0530

    detction of correct face V1

--- detect_face.py
+++ detect_face.py
@@ -1,4 +1,5 @@
 from mtcnn.mtcnn import MTCNN
+import cv2
 # detect face
 #     |-detect all faces
 #     |-get the required face
@@ -13,7 +14,8 @@ class DetectFace():
         max_face = (0,0,0,0)
 
         for face in faces:
-            x, y, w, h = face['box']
+            # x, y, w, h = face['box']
+            x, y, w, h = face                               # haar cascade
             area = w*h
             if max_area < area:
                 max_area = area
@@ -23,15 +25,21 @@ class DetectFace():
 
 
     def detect_all_faces(self):
-        detector    = MTCNN()                               # using the detector best fit (move to config)
-        faces       = detector.detect_faces(self.img)       # detect all the faces available
-        
+        # detector    = MTCNN()                               # using the detector best fit (move to config)
+        # faces       = detector.detect_faces(self.img)       # detect all the faces available
+        face_cascade    = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        gray            = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
+        faces           = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+
         return faces
 
 
     def detect_req_face(self):
         faces   = self.detect_all_faces()
-        face    = self.required_face(faces)
+        face = (0,0,0,0)
+        if len(faces) != 0:
+            face    = self.required_face(faces)
 
         return face
 
